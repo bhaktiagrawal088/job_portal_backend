81ae1799aa494c5d35fca907f282e5ca1eb64773
unknown <bhaktiagrawal397@gmail.com>
2024-12-23 12:44:20 +0530

    update

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOption = {
   origin: [
-  'http://localhost:5173',
-  'https://classy-haupia-aed958.netlify.app'],
+  'https://localhost:5173','https://classy-haupia-aed958.netlify.app'],
   credentials: true,
 };
 app.use(cors(corsOption));
