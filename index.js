d49593404fe9ee0c2f5e0013ac98ed53cbb0c0e0
unknown <bhaktiagrawal397@gmail.com>
2024-12-23 11:24:12 +0530

    update

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: "https://job-portal-backend-whco.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOption));
